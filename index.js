820a41d42ee2a7a6051b421eff9b7497d207432c
Elizabeth <e.bellingham02@gmail.com>
2023-05-21 12:10:30 -0400

    Created an array for weekdays using fake data and looped them.

--- index.js
+++ index.js
@@ -47,29 +47,22 @@ function displayForecast() {
   let forecastElement = document.querySelector("#weather-forecast");
 
   let forecastHTML = `<div class="row" id="weekdays">`;
-  forecastHTML =
-    forecastHTML +
-    `
+  let days = ["Thur", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
   <div class="col-2">
-    <div class="weather-forecast-date">Monday</div>
-    <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="36" />
+    <div class="weather-forecast-date">${day}</div>
+    <img src="http://openweathermap.org/img/wn/50d@2x.png"
+     alt=""
+      width="42" />
     <div class="weather-forecast-temperature">
       <span class="weather-forecast-temperature-max">18° </span>
       <span class="weather-forecast-temperature-min">12°</span>
     </div>
   </div>`;
-
-  forecastHTML =
-    forecastHTML +
-    `
-  <div class="col-2">
-    <div class="weather-forecast-date">Monday</div>
-    <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="36" />
-    <div class="weather-forecast-temperature">
-      <span class="weather-forecast-temperature-max">18° </span>
-      <span class="weather-forecast-temperature-min">12°</span>
-    </div>
-  </div>`;
+  });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
